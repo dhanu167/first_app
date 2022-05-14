2a6f0bd2d438a24bc42c62494ae19813784e2ab0
V, Dhanush <Dhanush.V@cerner.com>
2022-05-14 16:47:13 +0530

    api https update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
 
-const API_URL = 'http://www.omdbapi.com?apikey=77117f3a';
+const API_URL = 'https://www.omdbapi.com?apikey=77117f3a';
 
 
 const App = () => {
